9294c60251d5cb99bfd075e323735b37cdbd451b
cantstopcoding <jonathan_koss@yahoo.com>
2022-02-02 22:47:34 -0500

    update variable names and test block description

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,17 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("name added once submit button is clicked", () => {
+test("Add todo item to list", () => {
   render(<App />);
 
   const inputElement = screen.getByPlaceholderText(/Add todo here.../i);
-  const submitButton = screen.getByRole("button", {
+  const buttonElement = screen.getByRole("button", {
     name: /add/i,
   });
   fireEvent.change(inputElement, { target: { value: "Meditate" } });
-  fireEvent.click(submitButton);
+  fireEvent.click(buttonElement);
 
-  const divElement = screen.getByText(/Meditate/i);
-  expect(divElement).toBeInTheDocument();
+  const listItemElement = screen.getByText(/Meditate/i);
+
+  expect(listItemElement).toBeInTheDocument();
 });
